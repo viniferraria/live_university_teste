145d6b4db4d3a339d9cc156dfb25e68eda7944fa
Vinícius Amendoeira Ferraria <39192461+viniferraria@users.noreply.github.com>
2021-05-15 01:22:34 +0000

    testando tp

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 const express = require("express");
 const app = express();
 var tp = require('tedious-promises');
-var dbConfig = require('./config.json');
 var TYPES = require('tedious').TYPES;
+var dbConfig = require('./config.json');
 tp.setConnectionConfig(dbConfig); 
-// tp.setPromiseLibrary('es6');
+tp.setPromiseLibrary('es6');
 
 app.use(express.urlencoded({
     extended: true
@@ -14,19 +14,8 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.set('view engine', 'ejs');
 
-let people = ['geddy', 'neil', 'alex'];
-
-/* let html = ejs.render('<ul> \
-<% people.forEach(function (person) { %> \
-<li><%- person _%> \
-</li> \
-<% }); %> \
-</ul>', { people });
-console.log(html) */
-
-
 app.get('/', (_, res) => {
-    res.render('index', { people });
+    res.render('index'/*, { people }*/);
 });
 
 app.post('/', (req, res) => {
@@ -35,15 +24,28 @@ app.post('/', (req, res) => {
     return res.status(200).json({ nome, sobrenome, email });
 });
 
-app.get('/query', (_, res) => {
-    tp.sql("SELECT TOP 10 * FROM dbo.tbs_nome")
+app.get('/query', async (_req, res) => {
+    let message, status;
+    try {
+      let results = await tp.sql("SELECT TOP 10 * FROM dbo.tbs_nome").execute();
+      console.log(results);
+      status = 200;
+      message = "Sucesso";
+    } catch (err) {
+      console.log(err);
+      message = err.toString();
+      status = 404;
+    } finally {
+      return res.status(status).json({ message });
+    }
+    /* tp.sql("SELECT TOP 10 * FROM dbo.tbs_nome")
     .execute()
     .then(function(results) {
         console.log(results);
         return res.status(200).json({ nome, sobrenome, email });
     }).fail(function(err) {
-        return res.status(400).json({ "erro": "erro" });
-    });
+      return res.status(400).json({ "erro": "erro" });
+    }); */
 });
 
 
